426985c59c5184e266a6e32299076626b44c2ab2
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-11-23 12:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-//your JS code here. If required.
-async function delayAndDisplayMessage() {
-  // Get user input
-  const textInput = document.getElementById('text').value;
-  const delayInput = document.getElementById('delay').value;
+//your JS code here. If required
+// Get the required elements from the DOM
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const submitButton = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
 
-  // Validate inputs
-  if (!textInput || isNaN(delayInput) || delayInput <= 0) {
-    alert('Please enter valid input values.');
+// Define the async function
+async function showMessageWithDelay() {
+  // Get the text and delay values from the user inputs
+  const text = textInput.value;
+  const delay = parseInt(delayInput.value, 10);
+
+  // Validate the inputs
+  if (!text || isNaN(delay) || delay <= 0) {
+    outputDiv.innerText = 'Invalid input';
     return;
   }
 
-  // Convert delay to milliseconds
-  const delayMilliseconds = delayInput * 1000;
-
-  // Use async/await to wait for the specified delay
-  await new Promise(resolve => setTimeout(resolve, delayMilliseconds));
+  // Wait for the specified delay
+  await new Promise(resolve => setTimeout(resolve, delay));
 
   // Display the message on the webpage
-  const outputDiv = document.getElementById('output');
-  outputDiv.textContent = textInput;
+  outputDiv.innerText = text;
 }
 
-// Attach the function to the button click event
-document.getElementById('btn').addEventListener('click', delayAndDisplayMessage);
-
+// Attach event listener to the submit button
+submitButton.addEventListener('click', showMessageWithDelay);
